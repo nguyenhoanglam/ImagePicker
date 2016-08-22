7c0c3e3039f001b313b480bfa47611e0eb55398e
NGUYEN HOANG LAM <hoanglamvn90@gmail.com>
2016-08-22 09:07:13 +0700

    Fix no images return error, allow to change picker's title

--- imagepicker/build/generated/source/r/debug/android/support/design/R.java
+++ imagepicker/build/generated/source/r/debug/android/support/design/R.java
@@ -825,18 +825,18 @@ public final class R {
 		public static final int abc_activitychooserview_choose_application = 0x7f060007;
 		public static final int abc_capital_off = 0x7f060008;
 		public static final int abc_capital_on = 0x7f060009;
-		public static final int abc_font_family_body_1_material = 0x7f060023;
-		public static final int abc_font_family_body_2_material = 0x7f060024;
-		public static final int abc_font_family_button_material = 0x7f060025;
-		public static final int abc_font_family_caption_material = 0x7f060026;
-		public static final int abc_font_family_display_1_material = 0x7f060027;
-		public static final int abc_font_family_display_2_material = 0x7f060028;
-		public static final int abc_font_family_display_3_material = 0x7f060029;
-		public static final int abc_font_family_display_4_material = 0x7f06002a;
-		public static final int abc_font_family_headline_material = 0x7f06002b;
-		public static final int abc_font_family_menu_material = 0x7f06002c;
-		public static final int abc_font_family_subhead_material = 0x7f06002d;
-		public static final int abc_font_family_title_material = 0x7f06002e;
+		public static final int abc_font_family_body_1_material = 0x7f060022;
+		public static final int abc_font_family_body_2_material = 0x7f060023;
+		public static final int abc_font_family_button_material = 0x7f060024;
+		public static final int abc_font_family_caption_material = 0x7f060025;
+		public static final int abc_font_family_display_1_material = 0x7f060026;
+		public static final int abc_font_family_display_2_material = 0x7f060027;
+		public static final int abc_font_family_display_3_material = 0x7f060028;
+		public static final int abc_font_family_display_4_material = 0x7f060029;
+		public static final int abc_font_family_headline_material = 0x7f06002a;
+		public static final int abc_font_family_menu_material = 0x7f06002b;
+		public static final int abc_font_family_subhead_material = 0x7f06002c;
+		public static final int abc_font_family_title_material = 0x7f06002d;
 		public static final int abc_search_hint = 0x7f06000a;
 		public static final int abc_searchview_description_clear = 0x7f06000b;
 		public static final int abc_searchview_description_query = 0x7f06000c;
@@ -846,9 +846,9 @@ public final class R {
 		public static final int abc_shareactionprovider_share_with = 0x7f060010;
 		public static final int abc_shareactionprovider_share_with_application = 0x7f060011;
 		public static final int abc_toolbar_collapse_description = 0x7f060012;
-		public static final int appbar_scrolling_view_behavior = 0x7f06002f;
-		public static final int bottom_sheet_behavior = 0x7f060030;
-		public static final int character_counter_pattern = 0x7f060031;
+		public static final int appbar_scrolling_view_behavior = 0x7f06002e;
+		public static final int bottom_sheet_behavior = 0x7f06002f;
+		public static final int character_counter_pattern = 0x7f060030;
 		public static final int status_bar_notification_info_overflow = 0x7f060013;
 	}
 	public static final class style {
